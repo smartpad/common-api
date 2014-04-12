25461e1f901d9e5e8efca3c6afef9f6a6639c27e
Huy Banh <huy.banh@jinnova.com>
2014-04-12 17:09:52 +0700

    store driller

--- src/main/java/com/jinnova/smartpad/partner/IDetailManager.java
+++ src/main/java/com/jinnova/smartpad/partner/IDetailManager.java
@@ -6,7 +6,21 @@ public interface IDetailManager {
 	
 	static int TYPE_BRANCH = 0;
 	
-	static int TYPE_COUNT = 1;
+	static int TYPE_STORE = 1;
+	
+	static int TYPE_COUNT = 2;
+	
+	static String TYPENAME_BRANCH = "branch";
+	
+	static String TYPENAME_STORE = "store";
+	
+	static String TYPENAME_CAT = "cat";
+	
+	static String TYPENAME_CATITEM = "ci";
+	
+	static String TYPENAME_PROMO = "pro";
+	
+	static String TYPENAME_POST = "post";
 
 	String getDetail(int targetType, String targetId, String gpsLon, String gpsLat, int page) throws SQLException;
 	
