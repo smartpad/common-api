04e2b50df011f2616f16642e29a3b223e711b397
huybanh <huy.banh@jinnova.com>
2014-04-17 16:49:25 +0700

    sizing/flatten in drilling

--- src/main/java/com/jinnova/smartpad/partner/IDetailManager.java
+++ src/main/java/com/jinnova/smartpad/partner/IDetailManager.java
@@ -4,6 +4,9 @@ import java.sql.SQLException;
 
 public interface IDetailManager {
 	
+	static final String FIELD_TYPE = "type";
+	static final String FIELD_ARRAY = "arr";
+	
 	static int TYPE_BRANCH = 0;
 	
 	static int TYPE_STORE = 1;
@@ -22,7 +25,10 @@ public interface IDetailManager {
 	
 	static String TYPENAME_POST = "post";
 	
-	static String TYPENAME_COMPOUND = "com";
+	static String TYPENAME_COMPOUND_BRANCHSTORE = "com.branch";
+	static String TYPENAME_COMPOUND_CAT = "com.cat";
+	static String TYPENAME_COMPOUND_CITEM = "com.citem";
+	static String TYPENAME_COMPOUND_PROMOS = "com.promo";
 
 	String getDetail(int targetType, String targetId, String gpsLon, String gpsLat, int page) throws SQLException;
 	
