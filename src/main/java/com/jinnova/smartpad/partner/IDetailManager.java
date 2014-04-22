a787c9fc97bd18a2a3b3a308bc757fee2b876b76
huybanh <huy.banh@jinnova.com>
2014-04-22 11:01:08 +0700

    rest api revised

--- src/main/java/com/jinnova/smartpad/partner/IDetailManager.java
+++ src/main/java/com/jinnova/smartpad/partner/IDetailManager.java
@@ -25,6 +25,8 @@ public interface IDetailManager {
 	static String TYPENAME_PROMO = "pro";
 	static String TYPENAME_POST = "post";
 	static String TYPENAME_SYSCAT = "syscat";
+	
+	//static String[] TYPENAMES = new String[] {TYPENAME_BRANCH, TYPENAME_STORE, TYPENAME_CAT, TYPENAME_CATITEM, TYPENAME_PROMO};
 
 	static String TYPENAME_COMPOUND = "com.branch";
 	static String TYPENAME_COMPOUND_BRANCHSTORE = "com.branch";
@@ -32,9 +34,10 @@ public interface IDetailManager {
 	static String TYPENAME_COMPOUND_CITEM = "com.citem";
 	static String TYPENAME_COMPOUND_PROMOS = "com.promo";
 
-	String drill(int targetType, String targetId, String gpsLon, String gpsLat/*, int page, int size*/) throws SQLException;
+	String drill(String targetType, String targetId, String gpsLon, String gpsLat/*, int page, int size*/) throws SQLException;
 
 	String more(String targetType, String anchorType, String anchorId, String relation,
-			String gpsLon, String gpsLat, int offsete, int size) throws SQLException;
+			String branchId, String storeId, String catId, String syscatId, String excludeId,
+			String gpsLon, String gpsLat, int offset, int size) throws SQLException;
 	
 }
