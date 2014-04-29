15f0c23e46060fe687dffd2bae5896bd39ba733c
Huy Banh <huy.banh@jinnova.com>
2014-04-29 18:26:25 +0700

    drilling cat items

--- src/main/java/com/jinnova/smartpad/partner/IDetailManager.java
+++ src/main/java/com/jinnova/smartpad/partner/IDetailManager.java
@@ -16,7 +16,7 @@ public interface IDetailManager {
 	static final String FIELD_ACTION_LOADNEXT = "ALN";
 	
 	static String SYSTEM_BRANCH_ID = "smartpad";
-	
+	static String SYSTEM_CAT_ALL = "z";
 	static String CLUSPRE = "z";
 
 	//static int TYPE_NO = 0;
@@ -38,17 +38,17 @@ public interface IDetailManager {
 	static String TYPENAME_SYSCAT = "syscat";
 	
 	//static String[] TYPENAMES = new String[] {TYPENAME_NO, TYPENAME_BRANCH, TYPENAME_STORE, TYPENAME_CAT, TYPENAME_CATITEM, TYPENAME_PROMO};
-
-	static String TYPENAME_COMPOUND = "com.branch";
-	static String TYPENAME_COMPOUND_BRANCHSTORE = "com.branch";
-	static String TYPENAME_COMPOUND_CAT = "com.cat";
-	static String TYPENAME_COMPOUND_CITEM = "com.citem";
-	static String TYPENAME_COMPOUND_PROMOS = "com.promo";
+	//RULE: Singular type name + s
+	static String TYPENAME_COMPOUND = "misc";
+	static String TYPENAME_COMPOUND_BRANCHSTORE = "branchstore";
+	static String TYPENAME_COMPOUND_CAT = "cats";
+	static String TYPENAME_COMPOUND_CITEM = "citems";
+	static String TYPENAME_COMPOUND_PROMOS = "pros";
 
 	String drill(String uid, String targetType, String targetId, String targetSyscat, String gpsLon, String gpsLat/*, int page, int size*/) throws SQLException;
 
 	String more(int clusterId, String targetType, String anchorType, String anchorId, String relation,
 			String branchId, String storeId, String catId, String syscatId, String excludeId,
-			String gpsLon, String gpsLat, int offset, int size) throws SQLException;
+			boolean recursive, String gpsLon, String gpsLat, int offset, int size) throws SQLException;
 	
 }
